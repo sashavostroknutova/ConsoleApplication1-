820ca19f4216c7a87db0d74257b1cf17059a0a51
vostroknutovaay <vostroknutovaay@1X06X17>
2025-03-18 18:52:56 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication1]/ConsoleApplication1].cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "");
+	int m[10] = { 22, 33, 52, 12, 41, 97, 10, 2, 81, 11 };
+	int a;
+	cout << "Введите число для поиска в массиве";
+	cin >> a;
+	bool found = false;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		if (m[i] == a);
+		{
+			cout << "Число" << a << "Найдено в элементе с индщексом " << i << endl;
+			found = true;
+			break;
+		}
+	}
+	if (!found)
+	{
+		cout << "Число" << a << "Не найдено" << endl;
+	}
+}
